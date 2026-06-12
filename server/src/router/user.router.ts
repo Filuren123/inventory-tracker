0f16843de912a67e8b47e4a2e1cdf498ff31ae25
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2026-06-12 16:26:30 +0200

    Added authentication middleware and JSX base

--- server/src/router/user.router.ts
+++ server/src/router/user.router.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import { UserService } from "../service/user.service";
 import { User } from "../model/user.interface";
+import { authenticateToken } from "../middlewares/auth.middleware";
 
 const userService = new UserService();
+
+const jwt = require("jsonwebtoken");
 
 export const userRouter = express.Router();
 
@@ -17,6 +20,25 @@ userRouter.get(
             res.status(200).send(user);
         } catch (error: any) {
             res.status(500).send(error.message);
+        }
+    }
+);
+
+userRouter.post(
+    "/login",
+    async(
+    req: Request<{}, {}, { username: string; password: string }>,
+    res: Response<{ accessToken: string }>
+    ) => {
+        try {
+            // TODO: Authenticate user
+            
+            const username = req.body.username;
+            const user = { username: username };
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            res.json({ accessToken: accessToken });
+
+        } catch (error: any) {
         }
     }
 );
--- server/src/start.ts
+++ server/src/start.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 import { userRouter } from "./router/user.router";
 
 export const app = express();
-
+dotenv.config();
 app.use(express.json());
 
 app.use("/user", userRouter);
