badc9a45deba522bf54c243ff0f542ac163d4a52
Filip Lundberg <fillun0514@gmail.com>
2026-06-25 16:43:52 +0200

    P1F, Finished product row HTML/CSS

--- apps/web-client/src/components/product/Product.tsx
+++ apps/web-client/src/components/product/Product.tsx
@@ -3,9 +3,25 @@ import React from "react";
 
 const Product = () => {
     return (
-        <div>
-            
-        </div>
+        <article>
+            <div className="heading">
+                <h2>Kungsörnen</h2>
+                <div className="ean">EAN: 21310889324823</div>
+            </div>
+            <div className="firstsec">
+                <h1>Bakpulver</h1>
+                <div className="category">CATEGORY</div>
+            </div>
+            <div className="secsec">
+                <div className="bestbefore">
+                    Best before: 6 mars, 2026
+                </div>
+                <div>
+                    <button type="button" className="btn btn-primary">Se product</button>
+                    <button type="button" className="btn btn-danger">Remove</button>
+                </div>
+            </div>
+        </article>
     )
 }
 
