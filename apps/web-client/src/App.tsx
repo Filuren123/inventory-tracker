c511a0614c0419a25b90f465dcebcdfce19491a0
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2026-06-25 14:35:59 +0200

    P1W, Added product row to main page

--- apps/web-client/src/App.tsx
+++ apps/web-client/src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Product from './components/product/Product'
 
 function App() {
   return (
     <>
       <main>
+        <aside>
+          
+        </aside>
+        <section>
+          <Product />
+          <Product />
+          <Product />
+        </section>
       </main>
     </>
   )
