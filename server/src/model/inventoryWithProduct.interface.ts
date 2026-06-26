aba78adb0f5def7e9180d29f0cd00720ce516512
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2026-06-26 15:22:08 +0200

    Added interface that combines inventory props and product

---
+++ server/src/model/inventoryWithProduct.interface.ts
@@ -0,0 +1,6 @@
+import { Inventory } from "./inventory.interface";
+import { Product } from "./product.interface";
+
+export interface InventoryWithProduct extends Inventory {
+    product: Product;
+}
