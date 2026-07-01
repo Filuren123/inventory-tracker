c05b4761357d3c2341b133d54bdccfadd34ebdaa
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2026-07-01 20:30:10 +0200

    Added add/remove category to the api file

--- apps/web-client/src/api/categories.ts
+++ apps/web-client/src/api/categories.ts
@@ -11,3 +11,20 @@ export async function getAllCategories(): Promise<Category[]> {
         throw new Error(err.response.data);
     }
 }
+
+export async function addCategory(name: string, parent_id?: number): Promise<Category> {
+    try {
+        const res = await axios.post(`${baseURL}/categories/add`, {name, parent_id});
+        return res.data;
+    } catch (err: any) {
+        throw new Error(err.response.data);
+    }
+}
+
+export async function removeCategory(categoryId: number): Promise<void> {
+    try {
+        await axios.delete(`${baseURL}/categories/${categoryId}`);
+    } catch (err: any) {
+        throw new Error(err.response.data);
+    }
+}
