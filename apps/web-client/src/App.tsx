7a2002863442712a3b83b00288d38def3f31acf6
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2026-07-02 18:21:44 +0200

    Fixed routes in App.tsx

--- apps/web-client/src/App.tsx
+++ apps/web-client/src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
 import InventoryPage from './components/inventory/inventoryPage/InventoryPage';
+import InventoryAddRem from './components/inventory/inventoryAddRem/InventoryAddRem';
 
 function App() {
 	return (
 		<>
-			<InventoryPage />
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						element={<InventoryPage />}
+					/>
+					<Route
+						path="addItem"
+						element={<InventoryAddRem />}
+					/>
+				</Routes>
+			</Router>
 		</>
 	)
 }
