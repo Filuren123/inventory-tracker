edd79de0ddd5c41b1797fe569287dcdc75429f58
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2026-07-13 18:48:05 +0200

    More compiler options

--- server/src/start.ts
+++ server/src/start.ts
@@ -3,12 +3,18 @@ import dotenv from 'dotenv';
 import dotenvFlow from 'dotenv-flow';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 
 import { userRouter } from './router/user.router.js';
 import { inventoryRouter } from './router/inventory.router.js';
 import { productRouter } from './router/product.router.js';
 import { categoryRouter } from './router/category.router.js';
 import { authenticateToken } from './middlewares/auth.middleware.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenvFlow.config();
 
@@ -33,7 +39,12 @@ app.use((req, res, next) => {
 
 app.use('/user', userRouter);
 
-app.use(authenticateToken); // Apply authentication middleware to all routes below
-app.use('/inventory', inventoryRouter);
-app.use('/product', productRouter);
-app.use('/categories', categoryRouter);
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/inventory', authenticateToken, inventoryRouter);
+app.use('/product', authenticateToken, productRouter);
+app.use('/categories', authenticateToken, categoryRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
